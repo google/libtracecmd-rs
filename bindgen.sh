ce552bdb6f98d166645e27b58c857019bf2d1e75
Keiichi Watanabe <keiichiw@chromium.org>
2023-05-23 18:33:12 +0900

    Regenerate bindings.sh

--- bindgen.sh
+++ bindgen.sh
@@ -21,13 +21,26 @@ if [[ $# -ne 1 ]] || [[ $(basename "$1") != 'trace-cmd.h' ]]; then
     exit 1
 fi
 
-echo "
+echo '// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #![allow(clippy::upper_case_acronyms)]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
-" > ./src/bindings.rs
+' > ./src/bindings.rs
 
 bindgen \
     --no-layout-tests \

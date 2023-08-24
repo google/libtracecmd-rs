d27ae6c17ee07ec9ef8a9e63785dea1087403d22
Natsuko Mineno <natsukomineno@chromium.org>
2023-08-24 01:27:11 +0000

    "modify print_fields" "add explaining of print_fields()" "do cargo fmt and cargo clippy"

--- src/lib.rs
+++ src/lib.rs
@@ -213,22 +213,15 @@ impl Event {
     /// This is a wrapper of
     /// [tep_record_print_fields](https://www.trace-cmd.org/Documentation/libtraceevent/libtraceevent-field_print.html).
     pub fn print_fields(&self, rec: &Record) {
+        println!("fields: {:?}", self.get_fields(rec));
+    }
+    /// Get each field name follwed by the record's field value according to the field's type.
+    ///
+    /// This is a wrapper of
+    /// [tep_record_print_fields](https://www.trace-cmd.org/Documentation/libtraceevent/libtraceevent-field_print.html).
+    pub fn get_fields(&self, rec: &Record) -> String {
         let mut seq: bindings::trace_seq = Default::default();
         unsafe {
-            bindings::trace_seq_init(&mut seq);
-            bindings::trace_seq_reset(&mut seq);
-
-            bindings::tep_record_print_fields(&mut seq, rec.0, self.ptr);
-            bindings::trace_seq_terminate(&mut seq);
-        };
-        let msg = unsafe { std::slice::from_raw_parts(seq.buffer as *mut u8, seq.len as usize) };
-        println!("fields: {:?}", std::str::from_utf8(msg).unwrap());
-    }
-    ///This is a wrapper of
-    /// [tep_record_print_fields](https://www.trace-cmd.org/Documentation/libtraceevent/libtraceevent-field_print.html).
-    pub fn get_fields(&self, rec:&Record) -> String{
-        let mut seq: bindings::trace_seq = Default::default();
-        unsafe{
             bindings::trace_seq_init(&mut seq);
             bindings::trace_seq_reset(&mut seq);
 
